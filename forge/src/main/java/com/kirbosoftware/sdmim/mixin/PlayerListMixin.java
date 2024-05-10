9e1076e0c0d18492d4804a15b21903b03c0f0be7
Joost <joostmsoftware@gmail.com>
2024-05-10 14:57:10 +0200

    version 0.1.2:
    - Fixed forge mixin from booting up due to SDMIM.
    - bumped version

---
+++ forge/src/main/java/com/kirbosoftware/sdmim/mixin/PlayerListMixin.java
@@ -0,0 +1,15 @@
+package com.kirbosoftware.sdmim.mixin;
+
+import net.minecraft.network.Connection;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
+    @Redirect(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Connection;getLoggableAddress(Z)Ljava/lang/String;"))
+    private String sdmim$placeNewPlayer(Connection instance, boolean bl) {
+        return "***.***.***.***:*****";
+    }
+}
